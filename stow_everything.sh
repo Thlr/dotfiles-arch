d77227bbd12194bf27f1c9df02e4f2b596e49c93
Théo Larue <thlarue@live.fr>
2019-12-25 17:34:10 +0100

    stow everything script

---
+++ stow_everything.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Do not include stow in that list. It is loaded beforehand anyways
+apps="compton dunst git i3 kitty nvim p10k polybar ranger rofi wpg zsh"
+
+OPTS=$@
+
+stow $OPTS stow
+for app in $apps
+do
+    stow $OPTS $app
+done
