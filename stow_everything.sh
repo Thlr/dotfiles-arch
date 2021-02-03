9024c091ecabe12c70dd128d4ea4810077aa7f61
Théo Larue <theo.larue@ryax.tech>
2021-02-03 21:20:22 +0100

    added fonts to the dotfiles

--- stow_everything.sh
+++ stow_everything.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apps="dunst git kitty nix nvim p10k ranger rofi zathura zsh tmux x11 picom scripts shell"
+apps="dunst git kitty nix nvim p10k ranger rofi zathura zsh tmux x11 picom scripts shell fonts"
 
 OPTS="--no-folding --verbose=1"
 
